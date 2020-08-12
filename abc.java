b25ec889d3a453d24446b51de782f174016bca3d
Shivam Sharma <shiv16085@gmail.com>
2020-08-12 05:44:53 +0000

    2nd commit

--- abc.java
+++ abc.java
@@ -3,9 +3,14 @@ public class abc {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         name();
+        name1();
     }
 
     public static void name() {
+    System.out.println("HI ji");
+    }
+
+    public static void name1() {
     System.out.println("HI");
     }
 }
